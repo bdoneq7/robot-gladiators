1ab3d7fd41a2728957984dac48b075e7016e21b6
Brian Done <briguy2078@gmail.com>
2021-10-25 15:18:36 -0400

    multiple rounds complete

--- assets/js/game.js
+++ assets/js/game.js
@@ -69,6 +69,7 @@ var fight = function(enemyName) {
     } else {
       window.alert(playerName + ' still has ' + playerHealth + ' health left.');
     }
+
   }
     
 }; // fight function ends
@@ -77,5 +78,12 @@ for (var i = 0; i < enemyNames.length; i++) {
   var pickedEnemyName = enemyNames[i];
   enemyHealth = 50;
   fight(enemyNames[i]);
+  if (playerHealth > 0) {
+    window.alert("Welcome to Robot Gladiators! Round" + (i + 1));
+  }
+  else {
+    window.alert("You have lost your robot in battle! Game Over!");
+    break;
+  }
   
 }
